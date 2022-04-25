5bc1f92f3a0f7ba82ff9b404b6c8e14c05b72236
LourenceQ <lqfmecatronica@gmail.com>
2022-04-25 18:33:37 -0300

    ex8

--- Trabalho8-04-2022/ex8/src/Entities/Market.java
+++
@@ -1,9 +0,0 @@
-package Entities;
-
-import java.util.ArrayList;
-import Enum.PaymentEnum;
-
-public class Market {
-    public PaymentEnum Payment;
-    public ArrayList<Product> Order = new ArrayList<Product>();    
-}
--- Trabalho8-04-2022/ex8/src/Main.java
+++ Trabalho8-04-2022/ex8/src/Main.java
@@ -1,21 +1,21 @@
-import Entities.Market;
-import Entities.Product;
+import Entities.Contact;
+import Entities.Schedule;
 
 class Main {
     public static void main(String[] args) {
-        Product pd1 = new Product("Queijo", 35, 3);
-        Product pd2 = new Product("Arroz", 12, 4);
-        Product pd3 = new Product("Feijão", 15, 3);
+        Contact ct1 = new Contact("Ricardo", "35 987786547","Masculino" ,40);
+        Contact ct2 = new Contact("João","33 9889786547" , "Masculino", 54);
+        Contact ct3 = new Contact("Heitor", "31 987786547", "Masculino", 35);
 
-        Market mk = new Market();
-        mk.Order.add(pd1);
-        mk.Order.add(pd2);
-        mk.Order.add(pd3);
+        Schedule mk = new Schedule();
+        mk.Contact.add(ct1);
+        mk.Contact.add(ct2);
+        mk.Contact.add(ct3);
 
-        int len = mk.Order.size();
+        int len = mk.Contact.size();
         for(int i=0; i<len; i++) {
-            System.out.println("Pedido nº " + (i+1));
-            System.out.println(mk.Order.get(i).toString());
+            System.out.println("Contato nº " + (i+1));
+            System.out.println(mk.Contact.get(i).toString());
             System.out.println();
         }
     }
